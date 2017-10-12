7925386e48882b14487752cca7e434a315b0999d
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-10-12 10:18:38 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['cloudwatch-logs']['region'] = 'ap-southeast-2'
-default['cloudwatch-logs']['log_stream_name'] = '{instance_id}'
+default['cloudwatch-logs']['region'] = 'us-west-2'
+default['cloudwatch-logs']['log_stream_name'] = '{instance_id}-ES-syslog-LogStream'
