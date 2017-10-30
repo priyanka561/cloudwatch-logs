96b296c3383ea6bc1dbd656c930703554987e9aa
priyanka561 <30716140+priyanka561@users.noreply.github.com>
2017-10-30 13:06:37 +0530

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,9 @@
 name 'cloudwatch-logs-chef'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
 license 'All rights reserved'
 description 'Installs/Configures cloudwatch-logs'
 long_description 'Installs/Configures cloudwatch-logs'
 version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
